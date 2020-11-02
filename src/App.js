d6d12f344592ce7ceee4b84222665b7f27693c27
Vetty <vetty88@gmail.com>
2020-11-02 16:36:28 +1100

    updated src folder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Jumbotron from "./components/Jumbotron";
+import EmpList from './components/EmpList'
 
 function App() {
-  return (
-    <Router>
-      <div>
+    return (
+      <div className="container">
         <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
+        <Jumbotron />
+        <EmpList />
       </div>
-    </Router>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-=======
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> b77b8f60decbd9abb60d68b271157e0417d5cd54
 
 ReactDOM.render(<App />, document.getElementById('root'));
